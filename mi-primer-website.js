6ee73bebf628116fb1e0684a3138785f3eff44cf
fabibo1974 <fabian_bosio@hotmail.com>
2023-05-16 22:39:58 -0300

    Initial commit

---
+++ mi-primer-website.js
@@ -0,0 +1,48 @@
+ 
+document.querySelector("button.button-menu-toggle")
+      .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                        classList.toggle("nav-links-responsive")})                     
+
+ /*                     
+
+                      function showalert () {
+                        alert ("¡Hola! Esta es una alerta de Javascript")
+                      }
+                      
+                      const form = document.getElementById('myForm');
+                      
+                      function validateEmail(email) {
+                        const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+                        return regex.test(email)
+                      }
+                      
+                      function validateForm() {
+                        const emailInput = document.getElementById('email');
+                        const email = emailInput.value;
+                      
+                        if (!validateEmail(email)) {
+                          alert('Por favor ingrese un correo electrónico válido.');
+                        } else {
+                          alert('Correo electrónico enviado correctamente.');
+                        }
+                      }
+                      
+                      form.addEventListener('submit', function(event) {
+                        event.preventDefault();
+                        validateForm();
+                      })
+                    
+                     document.querySelector("button.button-menu-toggle")
+                    .addEventListener("click", function() {
+                    document.querySelector(".nav-links").
+                    classList.toggle("nav-links-responsive")})
+                    Alejandra Arribillaga  to  Everyone 10:47 PM
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Mi Primer Website</title>
+                        <link rel="stylesheet" href="estilos8.css">
+                    </head>       
+                    */             
